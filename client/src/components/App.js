e590bb7ef69faa2f074075d12d022a3e0d83b4b7
Peter T <ptumulty923@gmail.com>
2018-05-29 18:16:18 -0400

    style: added written content

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -64,18 +64,27 @@ class App extends Component {
               <FaPlusSquareO onClick={() => this.openModal()} />
               <div className="card">
                 <img className="card-image-top img-fluid mb-1" src={rpFeatured} alt="rachel peter header" />
-                <h1 className="text-center save-the-date-title">Save The Date <em><small>- 03/30/19</small></em></h1>
-                <h1 className="text-center mb-2">Our Love Story!</h1>
-                <LoginModal
-                  visible={this.state.visible}
-                  closeModal={this.closeModal}
-                  authenticateUser={this.authenticateUser}
-                />
+                <h1 className="text-center mt-3">Save The Date!</h1>
+                <h3 className="text-center mt-2 final-fold-message"><small>Celebrate love, laughter, and happily ever after!</small></h3>
+                <h3 className="text-center final-fold-message"><small>Save the date because Peter and Rachel are getting married</small></h3>
+                <h4 className="text-center final-fold-message"><strong>03/03/2019 Dayton, NJ</strong></h4>
+                <h5 className="text-center mt-3 final-fold-message">~ Formal Invitation to Follow ~</h5>
+                <h1 className="text-center mt-4">Our Love Story</h1>
                 <LoveStoryVideo />
+                <h3 class="text-center final-fold-message"><small>We kindly ask for your contact information<br/>so we best know where to send your invitation.</small></h3>
                 <AddGuest />
+                <h1 className="text-center mt-3">Contact Us</h1>
+
+                <h5 class="text-center final-fold-message">Any questions, comments, just want to say hello? Send us an email<br/>at <a href="mailto:tumultyeverafter@gmail.com">tumultyeverafter@gmail.com</a></h5>
+                <h6 class="text-center mb-4 mt-2 final-fold-message">Looking for our registry? Check out our Zola site <a href="https://zola.com">here.</a></h6>
               </div>
             </div>
           </div>
+          <LoginModal
+            visible={this.state.visible}
+            closeModal={this.closeModal}
+            authenticateUser={this.authenticateUser}
+          />
         </div>
       );
     }
