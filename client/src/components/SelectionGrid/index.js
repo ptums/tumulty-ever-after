a8471955b676b8e9b35b121518f7f9b62ed553de
Peter T <ptumulty923@gmail.com>
2019-01-13 10:29:37 -0500

    fixed links again'

--- client/src/components/SelectionGrid/index.js
+++ client/src/components/SelectionGrid/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Palace from "../../media/grid/the-palace.jpg";
 import Invitation from "../../media/grid/the-invitation.jpg";
 import PeterRachel from "../../media/grid/rachel-peter-cropped.jpg";
@@ -10,52 +11,52 @@ import "./index.css";
 const SelectionGrid = () => (
  <div className="SelectionGrid">
   <div className="cell cell-1"> 
-     <a href="/events" title={"events"}>
+     <Link to="/events" title={"events"}>
        <img src={Palace} alt="the palace" className="cell-image"/>
        <div className="cell-content">
         <p>What time is the Church? What time is the reception? All your answers here.</p>
        </div>
-     </a>
+     </Link>
   </div>
   <div className="cell cell-2">
-   <a href="/rsvp" title={"rsvp"}>
+   <Link to="/rsvp" title={"rsvp"}>
       <img src={Invitation} alt="rsvp" className="cell-image"/>
       <div className="cell-content">
          <p>We are looking forward to celebrating with you please let us know if you can make it.</p>
       </div>
-    </a>
+    </Link>
   </div>
   <div className="cell cell-3"> 
-    <a href="/about-us">
+    <Link to="/about-us">
        <img src={PeterRachel} alt="peter rachel" className="cell-image" />
        <div className="cell-content">
         <p>More about the happy couple.</p>
        </div>
-    </a>
+    </Link>
   </div>
   <div className="cell cell-4"> 
-    <a href="/accomodations">
+    <Link to="/accomodations">
        <img src={Hotel} alt="fairfield inn" className="cell-image" />
        <div className="cell-content">
         <p>Reserve your night(s) at the Fairfield Inn & Suites Marriot</p>
        </div>
-    </a>
+    </Link>
   </div>
   <div className="cell cell-5">
-   <a href="/registry">
+   <Link to="/registry">
       <img src={Flowers} alt="registry" className="cell-image" />
       <div className="cell-content">
         <p>Curious about our registry? Find out more here.</p>
       </div>
-    </a>
+    </Link>
   </div>
   <div className="cell cell-6"> 
-   <a href="/transportation">
+   <Link to="/transportation">
       <img src={Transportation} alt="transportation" className="cell-image" />
       <div className="cell-content">
         <p>How to get around!</p>
       </div>
-    </a>
+    </Link>
   </div>
  </div>
  
