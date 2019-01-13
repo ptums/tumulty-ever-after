d845b7a5e75d06b30f08932a0554b25d6fd4c552
Peter T <ptumulty923@gmail.com>
2019-01-13 10:25:06 -0500

    added new links

--- client/src/components/SelectionGrid/index.js
+++ client/src/components/SelectionGrid/index.js
@@ -10,7 +10,7 @@ import "./index.css";
 const SelectionGrid = () => (
  <div className="SelectionGrid">
   <div className="cell cell-1"> 
-     <a href="http://www.tumultyeverafter.com/events" title={"events"}>
+     <a href="/events" title={"events"}>
        <img src={Palace} alt="the palace" className="cell-image"/>
        <div className="cell-content">
         <p>What time is the Church? What time is the reception? All your answers here.</p>
@@ -18,7 +18,7 @@ const SelectionGrid = () => (
      </a>
   </div>
   <div className="cell cell-2">
-   <a href="http://www.tumultyeverafter.com/rsvp" title={"rsvp"}>
+   <a href="/rsvp" title={"rsvp"}>
       <img src={Invitation} alt="rsvp" className="cell-image"/>
       <div className="cell-content">
          <p>We are looking forward to celebrating with you please let us know if you can make it.</p>
@@ -26,7 +26,7 @@ const SelectionGrid = () => (
     </a>
   </div>
   <div className="cell cell-3"> 
-    <a href="http://www.tumultyeverafter.com/about-us">
+    <a href="/about-us">
        <img src={PeterRachel} alt="peter rachel" className="cell-image" />
        <div className="cell-content">
         <p>More about the happy couple.</p>
@@ -34,7 +34,7 @@ const SelectionGrid = () => (
     </a>
   </div>
   <div className="cell cell-4"> 
-    <a href="http://www.tumultyeverafter.com/accomodations">
+    <a href="/accomodations">
        <img src={Hotel} alt="fairfield inn" className="cell-image" />
        <div className="cell-content">
         <p>Reserve your night(s) at the Fairfield Inn & Suites Marriot</p>
@@ -42,7 +42,7 @@ const SelectionGrid = () => (
     </a>
   </div>
   <div className="cell cell-5">
-   <a href="http://www.tumultyeverafter.com/registry">
+   <a href="/registry">
       <img src={Flowers} alt="registry" className="cell-image" />
       <div className="cell-content">
         <p>Curious about our registry? Find out more here.</p>
@@ -50,7 +50,7 @@ const SelectionGrid = () => (
     </a>
   </div>
   <div className="cell cell-6"> 
-   <a href="http://www.tumultyeverafter.com/transportation">
+   <a href="/transportation">
       <img src={Transportation} alt="transportation" className="cell-image" />
       <div className="cell-content">
         <p>How to get around!</p>
